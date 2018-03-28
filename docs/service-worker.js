85f49b2948f24b5db45be49fa6274d6bd3d30861
Rahad Rahman <rahman.rahad@yahoo.com>
2018-03-28 15:22:48 +0600

    Update service-worker.js

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -75,3 +75,21 @@ self.addEventListener('fetch', event => {
     );
   }
 });
+
+window.addEventListener('beforeinstallprompt', function(e) {
+  // beforeinstallprompt Event fired
+
+  // e.userChoice will return a Promise.
+  // For more details read: https://developers.google.com/web/fundamentals/getting-started/primers/promises
+  e.userChoice.then(function(choiceResult) {
+
+    console.log(choiceResult.outcome);
+
+    if(choiceResult.outcome == 'dismissed') {
+      console.log('User cancelled home screen install');
+    }
+    else {
+      console.log('User added to home screen');
+    }
+  });
+});
