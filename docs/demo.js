54f7a82e33a676a899d034b1780c5df7eedf7539
Rahad Rahman <rahman.rahad@yahoo.com>
2018-03-28 17:47:19 +0600

    Update demo.js

--- docs/demo.js
+++ docs/demo.js
@@ -1,3 +1,9 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js');
-}
+  if ('serviceWorker' in navigator) {
+    console.log("Will the service worker register?");
+    navigator.serviceWorker.register('service-worker.js')
+      .then(function(reg){
+        console.log("Yes, it did.");
+      }).catch(function(err) {
+        console.log("No it didn't. This happened: ", err)
+      });
+  }
